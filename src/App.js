8a9ad2d11fd7dad6a00f4ac8a54654ac8ff3b09d
Ian <leandro.esparrago@snappydot.com>
2020-11-02 21:25:28 +0800

    Add files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const { register, handleSubmit, errors } = useForm();
+  const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+  };
+
+  const validateUsername = async (value) => {
+    await sleep(1000);
+
+    if (value === "bill") {
+      return true;
+    }
+
+    return false;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form className="App" onSubmit={handleSubmit(onSubmit)}>
+        <h1>Sign Up</h1>
+        <label>First Name:</label>
+        <input
+          name="firstName"
+          ref={register({ required: true, minLength: 2 })}
+        />
+        {errors.firstName && <p>This is required</p>}
+
+        <label>Last Name:</label>
+        <input name="lastName" ref={register({ required: true })} />
+        {errors.lastName && <p>This is required</p>}
+
+        <label>Gender</label>
+        <select name="gender" ref={register({ required: true })}>
+          <option value="">Select...</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+        {errors.gender && <p>This is required</p>}
+
+        <label>Username</label>
+        <input
+          name="username"
+          ref={register({ required: true, validate: validateUsername })}
+        />
+        {errors.username && <p>This is required</p>}
+
+        <label>Email</label>
+        <input name="email" ref={register({ required: true })} />
+        {errors.email && <p>This is required</p>}
+
+        <label>About you</label>
+        <textarea name="aboutYou" ref={register({ required: true })} />
+        {errors.aboutYou && <p>This is required</p>}
+
+        <input type="submit" />
+      </form>
     </div>
   );
 }
